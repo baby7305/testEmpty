482e956855170386d9c339667fa7b2f0f16bf741
baby7305 <songhaibei7305@163.com>
2018-04-08 16:47:44 +0800

    Controller加入系统时间，view加载出来

--- testEmpty/Controller/HomeController.cs
+++ testEmpty/Controller/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace testEmpty.Controllers
 {
@@ -7,6 +8,8 @@ namespace testEmpty.Controllers
         // GET
         public IActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewData["Greeting"] = hour < 12 ? "Good Moring" : "Good Afternoon";
             return View();
         }
     }
